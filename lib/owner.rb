7de81794983af1a6820f0334a5d7de425873efb3
Alexander Schelchere <ascellie@gmail.com>
2019-06-09 21:18:14 -0400

    class methods & #species - complete

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
 class Owner
-  # code goes here
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+
 end
